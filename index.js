74445eccd045fe922b359bcc156091c1da79e166
scottglz <grapes97223@gmail.com>
2015-12-28 20:11:56 -0800

    Documentation

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var defaultOptions = {
    cancelable: true
    //onDone
    //onCancel
-   //cancelable
 };
 
 function Animation(options) {
--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 var test = require("tape");
 var Animation = require("..");
 
